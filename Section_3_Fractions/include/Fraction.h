18f4d975876505885e61b71c2187be9d4195d508
Aarsh Chaube <65212523+Aarsh2001@users.noreply.github.com>
2024-01-19 15:59:51 +0000

    task 3 done

--- Section_3_Fractions/include/Fraction.h
+++ Section_3_Fractions/include/Fraction.h
@@ -1,2 +1,15 @@
 #pragma once
 #include <string>
+using namespace std;
+
+class Fraction{
+    public:
+    Fraction(int, int );
+    Fraction reciprocal();
+    Fraction multiply(int);
+    double toDouble();
+    string toString();
+
+    private:
+    int num; int denom;      
+};
--- Section_3_Fractions/src/Fraction.cpp
+++ Section_3_Fractions/src/Fraction.cpp
@@ -1,1 +1,26 @@
 #include "Fraction.h"
+using namespace std;
+
+Fraction::Fraction(int a, int b){
+   num = a;
+   denom = b;
+}
+Fraction Fraction::reciprocal(){
+    return Fraction(denom, num);
+}
+Fraction Fraction::multiply(int a){
+    return Fraction(num*a, denom);
+}
+double Fraction::toDouble(){
+    return (double)num/denom;
+}
+string Fraction::toString(){
+    if (num == 0)
+        {
+            return "0";
+        }
+        else
+        {
+            return to_string(num) + "/" + to_string(denom);
+        }
+}
